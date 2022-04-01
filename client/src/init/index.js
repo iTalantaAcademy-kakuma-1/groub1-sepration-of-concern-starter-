4af4e612e783ffd3622e96f87430ac63591b2355
Erick Aloise Josue <66235982+erickaloise1997@users.noreply.github.com>
2022-04-01 09:32:31 +0300

    update
    update

--- client/src/init/index.js
+++ client/src/init/index.js
@@ -1,19 +1,29 @@
 let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
+let editContainer = document.getElementById('edit');
+let deleteContainer = document.getElementById('delete');
+
+
+
 
 addToDoButton.addEventListener('click', function() {
+
+    var listElement = document.createElement('ol');
+    var listItem = document.createElement("li");
     var paragraph = document.createElement('p')
+    var editBtn = document.createElement('button')
+    var deleteBtn = document.createElement('button')
+
+    editBtn.innerText = "edit"
+    deleteBtn.innerText = "delete"
+
    
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
+    // paragraph.innerText = inputField.value;
+    // toDoContainer.appendChild(paragraph, editBtn, deleteBtn);
+    listItem.innerHTML += inputField.value +"  " + editBtn.outerHTML + "  "+ deleteBtn.outerHTML
+    listElement.appendChild(listItem);
+    toDoContainer.appendChild(listElement)
     inputField.value = "";
-
-    paragraph.addEventListener('click', function() {
-        paragraph.style.textDecoration = 'line-through';
-    })
-
-    paragraph.addEventListener('dblclick', function() {
-        toDoContainer.removeChild(paragraph)
-    })
+     
 })
